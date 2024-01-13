5df8de803b57b652019ecf3917212113f4fe314b
Tuhina Tripathi <tuhina.2313@gmail.com>
2024-01-13 15:53:26 -0500

    add script

--- script.js
+++ script.js
@@ -101,7 +101,7 @@ function displayLastPage() {
 
 function displayQuestion() {
     document.getElementById("pre-study").style.display = 'none';
-    document.getElementById("question-outer").style.display = 'none';
+    document.getElementById("question-outer").style.display = 'block';
 
     document.getElementById("Box1").style.display = 'block';
     document.getElementById("Box2").style.display = 'block';
