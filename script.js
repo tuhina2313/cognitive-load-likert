d5626d4b7713b0ad91ae78dc3a3ffefc31abb0a9
Tuhina Tripathi <tuhina.2313@gmail.com>
2024-01-16 12:54:05 -0500

    flex fix

--- script.js
+++ script.js
@@ -111,8 +111,8 @@ function displayQuestion() {
     document.getElementById("pre-study").style.display = 'none';
     document.getElementById("question-outer").style.display = 'block';
 
-    document.getElementById("Box1").style.display = 'block';
-    document.getElementById("Box2").style.display = 'block';
+    document.getElementById("Box1").style.display = 'flex';
+    document.getElementById("Box2").style.display = 'flex';
     var questionHeading = document.getElementById("question-heading");
     var questionContainer = document.getElementById("question-container");
     var optionsContainer = document.getElementById("options-container");
